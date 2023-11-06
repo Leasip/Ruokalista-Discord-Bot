cbc72d02fca0cd89f4fc9217985c0dd94c19b3d6
Mikxus <91991837+Mikxus@users.noreply.github.com>
2023-11-06 09:46:48 +0200

    fix: invalid decimal literal
    tokens are now stored as strings

--- src/setup.py
+++ src/setup.py
@@ -4,12 +4,12 @@ new_file_name = "src/config.py"
 print("This script will create the config file for the bot")
 print("Please remember that thiese are private information and should not be shared with anyone")
 print("This config file is only stored locally and is not uploaded to github")
-bot_token= input("please input your bot token: ")
-bot_token ="bot_token = "+ bot_token
-channel_id= input("please input the channel id you want to send the message to: ")
-channel_id ="channel_id = "+ channel_id
-debug_channel_id= input("please input the debug channel id: ")
-debug_channel_id ="debug_channel_id = "+ debug_channel_id
+bot_token= str(input("please input your bot token: "))
+bot_token ="bot_token = \"" + bot_token + "\""
+channel_id= str(input("please input the channel id you want to send the message to: "))
+channel_id ="channel_id = \"" + channel_id + "\""
+debug_channel_id= str(input("please input the debug channel id: "))
+debug_channel_id ="debug_channel_id = \"" + debug_channel_id + "\""
 
 print("Make sure to add the bot to the server and give it the required permissions")
 print("also check that the information you inputted is correct")
