f3ec8a37a9cb46b1744c868e889a31f6d51531f1
Mikxus <91991837+Mikxus@users.noreply.github.com>
2023-11-06 09:43:32 +0200

    update installer
    feat: uses python venv
    
    feat: runs discord token setup

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source config.sh
+
+echo Creating a venv
+mkdir venv
+python -m venv $venv_path
+
+echo Generating python requisites
+chmod u+x ./tools/generate_regs.sh
+./tools/generate_regs.sh
+
+$venv_pip install -r requirements.txt
+
+# Run secrets setup if config.py doesn't exist
+if [ ! -f config.py ]; then
+    $venv_python src/setup.py
+fi
+
+# then setup systemd timer
+sudo bash setup_systemd.sh
